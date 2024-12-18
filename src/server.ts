ece97ba89c9b683498fc400184d92a1f421b7a3e
brunopstephan <jogosdebruno123@gmail.com>
2024-12-18 12:10:08 -0300

    refactor: update ESLint configuration and improve code formatting in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,46 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
-import {fastifySwagger} from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import "dotenv";
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import 'dotenv'
 
 const port = Number(process.env.PORT) || 3333
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-app.register(fastifyCors, {origin: '*'})
+app.register(fastifyCors, { origin: '*' })
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(fastifySwagger, {
-    openapi: {
-        info: {
-            title: 'Fastify Template',
-            version: '1.0.0'
-        },
-        servers: [
-            {
-                url: `http://localhost:${port}`,
-                description: 'Development server'
-            }
-        ]
-    }
+  openapi: {
+    info: {
+      title: 'Fastify Template',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+        description: 'Development server',
+      },
+    ],
+  },
 })
 
 app.register(fastifySwaggerUi, {
-    routePrefix: '/docs',
+  routePrefix: '/docs',
 })
 
-app.listen({
-    port
-}).then(() => {
-    console.log(`Server is running on port ${port}`);
-})
+app
+  .listen({
+    port,
+  })
+  .then(() => {
+    console.log(`Server is running on port ${port}`)
+  })
